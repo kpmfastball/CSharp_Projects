4cd47ddd7a31c9f6866c7cf8dabfce87ee673913
kpmfastball <kpmfastball@gmail.com>
2019-03-10 18:03:55 -0500

    Initial commit of the GuessingGame and FoodBillCaclucatior Projects

---
+++ FoodBillCalculator/FoodBillCalculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Globalization;
+
+namespace FoodBillCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double billCost;
+            int numberOfPeople;
+            double tipPercentEntry;
+            bool invalidInput = true;
+
+            while (invalidInput)
+            {
+                try
+                {
+                    GetUserInput(out billCost, out numberOfPeople, out tipPercentEntry);
+                    Console.WriteLine(CalculateAmountOwed(billCost, numberOfPeople, tipPercentEntry));
+                    invalidInput = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a valid number");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("An unknown error occured");
+                }
+            }
+        }
+
+        private static void GetUserInput(out double billCost, out int numberOfPeople, out double tipPercentEntry)
+        {
+            Console.WriteLine("How much was the bill?");
+
+            billCost = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many people do you have in your party?");
+
+            numberOfPeople = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What percent would you like to tip?");
+
+            tipPercentEntry = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Each person owes:");
+        }
+
+        private static string CalculateAmountOwed(double billCost, int numberOfPeople, double tipPercentEntry)
+        {
+            double tipPercent = tipPercentEntry * .01f;
+            double tipAmount = tipPercent * billCost;
+            double amountOwed = (Math.Round((billCost + tipAmount) / numberOfPeople, 2));
+
+            string displayResult = amountOwed.ToString("c", new CultureInfo("en-US"));
+
+            return displayResult;
+        }
+    }
+}
---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GuessingGame
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+
+            int maxLimit;
+            int minLimit;
+            int factor;
+
+            getGameParameters(out maxLimit, out minLimit, out factor);
+
+            runGame(rnd, maxLimit, minLimit, factor);
+        }
+
+        private static void runGame(Random rnd, int maxLimit, int minLimit, int factor)
+        {
+            int correctAnswer;
+            int input;
+            int numberOfGuesses = 1;
+
+            do
+            {
+                do
+                {
+                    correctAnswer = rnd.Next(maxLimit);
+
+                }
+                while (correctAnswer < minLimit);
+            }
+            while (correctAnswer % factor != 0);
+
+            try
+            {
+
+                do
+                {
+                    input = Convert.ToInt32(Console.ReadLine());
+                    int divideRemainder = input % factor;
+
+                    if (input >= minLimit && input <= maxLimit)
+                    {
+                        if (divideRemainder == 0)
+                        {
+                            string guessPluralOption;
+
+                            if (numberOfGuesses > 1)
+                            {
+                                guessPluralOption = "guesses";
+                            }
+                            else
+                            {
+                                guessPluralOption = "guess";
+                            }
+
+                            if (input == correctAnswer)
+                            {
+                                Console.WriteLine("Correct. Way to go! It took you {0} valid {1} this time.", numberOfGuesses, guessPluralOption);
+                                numberOfGuesses++;
+                            }
+                            else if (input > correctAnswer)
+                            {
+                                Console.WriteLine("Too High! Try again. That is {0} valid {1} so far!", numberOfGuesses, guessPluralOption);
+                                numberOfGuesses++;
+                            }
+                            else if (input < correctAnswer)
+                            {
+                                Console.WriteLine("Too low! Give it another try. That is {0} valid {1} so far!", numberOfGuesses, guessPluralOption);
+                                numberOfGuesses++;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Pick a number divisible {0}, please!", factor);
+                        }
+                    }
+
+                    else
+                    {
+                        if (input > maxLimit)
+                        {
+                            Console.WriteLine("You cannot guess a number that is greater than {0}. Try again.", maxLimit);
+                        }
+
+                        else if (input < minLimit)
+                        {
+                            Console.WriteLine("You cannot guess a number less than {0}. Try again.", minLimit);
+                        }
+                    }
+                }
+                while (input < minLimit || input > maxLimit || input != correctAnswer);
+
+                Console.ReadKey();
+            }
+            catch (SystemException)
+            {
+                Console.WriteLine("Please enter a valid number");
+                runGame(rnd, maxLimit, minLimit, factor);
+            }
+        }
+
+        private static void getGameParameters(out int maxLimit, out int minLimit, out int factor)
+        {
+
+            try
+            {
+                Console.WriteLine("Pick a number to be the minimum of the guess range:");
+                minLimit = Convert.ToInt32(Console.ReadLine());
+
+                do
+                {
+                    Console.WriteLine("Pick a number to be the maximum of the guess range:");
+                    maxLimit = Convert.ToInt32(Console.ReadLine());
+
+                    if (maxLimit <= minLimit)
+                    {
+                        Console.WriteLine("Please choose a value that is greater than {0}, which you chose as your minimum", minLimit);
+                    }
+                }
+                while (maxLimit <= minLimit);
+
+                bool rangeIsValid;
+
+                do
+                {
+                    Console.WriteLine("What number should the answer be divisible by?:");
+                    factor = Convert.ToInt32(Console.ReadLine());
+                    rangeIsValid = maxLimit > factor;
+                    if (rangeIsValid == false)
+                    {
+                        Console.WriteLine("You must choose a number that is less than the difference between your minimum and maximum selections.");
+                    }
+                }
+                while (rangeIsValid == false);
+
+
+                Console.WriteLine("Pick an number between {0} and {1} that is divisible by {2}", minLimit, maxLimit, factor);
+            }
+            catch (SystemException)
+            {
+                Console.WriteLine("Please enter a valid number");
+                getGameParameters(out maxLimit, out minLimit, out factor);
+            }
+        }
+    }
+}
